66f9f475edf92fc0d2539413ee5d6569a4c21f41
Alex Peterson <aspeterson.126@gmail.com>
2023-08-25 17:04:58 -0500

    Added initial responsivity and positioning to all movies container

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import movieData from './mockData'
 import AllMovies from './components/AllMovies/AllMovies'
 
 export default function App() {
-  // const movieCards = mockData.map()
   const [movies, setMovies] = useState(movieData.movies)
-  console.log(movies)
 
   return (
     <>
