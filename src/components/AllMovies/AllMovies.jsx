c19f8cca912b60ef6521fffb6d951517d85c648b
Alex Peterson <aspeterson.126@gmail.com>
2023-08-25 17:08:01 -0500

    Render MovieCard components to AllMovies

--- src/components/AllMovies/AllMovies.jsx
+++ src/components/AllMovies/AllMovies.jsx
@@ -2,9 +2,17 @@ import MovieCard from '../MovieCard/MovieCard.jsx'
 
 export default function AllMovies(props) {
   const { movies } = props
+  console.log(movies)
 
   const movieCards = movies.map(movie => {
-    return <MovieCard title={movie.title} />
+    return (
+      <MovieCard
+        title={movie.title}
+        posterPath={movie.poster_path}
+        rating={movie.average_rating}
+        release={movie.release_date}
+      />
+    )
   })
 
   return <main className='all-movies-container'>{movieCards}</main>
