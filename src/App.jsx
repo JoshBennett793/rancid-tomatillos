445bca3240d5c0d99e745d00d55948978553c00d
Alex Peterson <aspeterson.126@gmail.com>
2023-08-25 20:55:21 -0500

    Add singleMovie component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import movieData from './mockData'
 import AllMovies from './components/AllMovies/AllMovies'
+import SingleMovie from './components/SingleMovie/SingleMovie'
 
 export default function App() {
   const [movies, setMovies] = useState(movieData.movies)
+  const [singleMovie, setSingleMovie] = useState(false)
+
+  const selectSingleMovie = id => {
+    const selectedMovie = movies.find(movie => movie.id === id)
+
+    return selectSingleMovie(selectedMovie)
+  }
 
   return (
     <>
+      <SingleMovie singleMovie={singleMovie} />
       <AllMovies movies={movies} />
     </>
   )
