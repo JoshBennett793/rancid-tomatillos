16928a5c54be4aa2cd9bf089a9832e3223313309
Josh Bennett <joshbennett793@yahoo.com>
2023-08-26 22:34:49 -0500

    Refactor data storage to pull from API movies endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import movieData from './mockData'
+import { useState, useEffect } from 'react'
 import AllMovies from './components/AllMovies/AllMovies'
 import SingleMovie from './components/SingleMovie/SingleMovie'
 
 export default function App() {
-  const [movies, setMovies] = useState(movieData.movies)
+  const [movies, setMovies] = useState([])
   const [selectedSingleMovie, setSelectedSingleMovie] = useState(null)
+  
+  const fetchMovieData = async () => {
+    const url = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+    try {
+      const response = await fetch(url)
+      
+      if (response.status === 500) {
+        throw new Error('There seems to be a problem. Please try refreshing your browser.')
+      }
 
-  const selectSingleMovie = id => {
-    const selectedMovie = movies.find(movie => movie.id === id)
+      const data = await response.json()
 
-    setSelectedSingleMovie(selectedMovie)
+      setMovies(data.movies)
+    } catch (error) {
+      console.log(error)
+    }
   }
+  
+    const selectSingleMovie = id => {
+      const selectedMovie = movies.find(movie => movie.id === id)
+  
+      setSelectedSingleMovie(selectedMovie)
+    }
+  
+    const returnAllMovies = () => {
+      setSelectedSingleMovie(null)
+    }
 
-  const returnAllMovies = () => {
-    setSelectedSingleMovie(null)
-  }
+  useEffect(() => {
+    fetchMovieData()
+  }, [])
 
   return (
     <>
